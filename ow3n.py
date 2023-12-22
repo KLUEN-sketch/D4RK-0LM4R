dff6a03fd2ac460b04650f466d78758098d5d625
KLUEN-sketch <154305684+KLUEN-sketch@users.noreply.github.com>
2023-12-22 16:06:56 +0800

    Add files via upload

---
+++ ow3n.py
@@ -0,0 +1,30 @@
+#WRITTEN BY : OWEN FUEZO
+#FOLLOW MY GITHUB : https://github.com/KLUEN-sketch/D4RK-0LM4R
+#----------import----------#
+import os
+#----------logo----------#
+logo=''                                                          
+  .g8""8q.`7MMF'     A     `7MF'`7MM"""YMM  `7MN.   `7MF'
+.dP'    `YM.`MA     ,MA     ,V    MM    `7    MMN.    M  
+dM'      `MM VM:   ,VVM:   ,V     MM   d      M YMb   M  
+MM        MM  MM.  M' MM.  M'     MMmmMM      M  `MN. M  
+MM.      ,MP  `MM A'  `MM A'      MM   Y  ,   M   `MM.M  
+`Mb.    ,dP'   :MM;    :MM;       MM     ,M   M     YMM  
+  `"bmmd"'      VF      VF      .JMMmmmmMMM .JML.    YM''')
+#----------clear----------#
+def clear():
+    os.system('clear')
+    print(logo)
+    print(42*'-')
+    print(' FACEBOOK : OWEN FUEZO ')
+    print(' GITHUB : https://github.com/KLUEN-sketch/D4RK-0LM4R')
+    print('42*'-')
+#----------menu----------#
+def main():
+	clear()
+	print(' [A] FILE CLONING ')
+	print(' [E] EXIT ')
+	option=input(' [??] CHOICE MENU : ')
+	
+#----------end----------#
+main()
